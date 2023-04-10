15cc68f6929de29668df851d157d9369c9eebce4
Sai Swaroopa :-) <125653378+Swaraa1211@users.noreply.github.com>
2023-04-10 23:12:12 +0530

    Connection.cs File Added

---
+++ Connection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLProject
+{
+    internal class Connection
+    {
+        public static string ConnString= "Data Source=5CG7324TYL;Initial Catalog = LMS_DB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        public static SqlConnection connection = new SqlConnection(ConnString);
+    }
+}
